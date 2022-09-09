1db73d57d483e2ced8a1eaa32c1cb0aed170232a
aniket-6234 <aniketashok1999@gmail.com>
2022-09-09 15:56:17 +0530

    GitHub Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import NotesIcon from "../images/file.svg"
+import React from "react";
+import NotesIcon from "../images/file.svg";
 import UserProfile from "../images/user.svg";
 
 const Navbar = () => {
@@ -9,11 +9,13 @@ const Navbar = () => {
         <img className="note-icon" src={NotesIcon} alt="icon" />
         <h1 className="heading">Keep Notes</h1>
       </div>
-      <div className="user">
-        <img className="icon" src={UserProfile} alt="icon" title="User" />
-      </div>
+      <a href="https://github.com/aniket-6234/Keep-Notes" target="_blank">
+        <div className="user">
+          <img className="icon" src={UserProfile} alt="icon" title="User" />
+        </div>
+      </a>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
